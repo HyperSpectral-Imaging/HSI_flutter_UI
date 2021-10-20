d33541f88fb798b705d6037bf17813ee36e5905a
Cheng Po Sheng <bencer@outlook.com>
2021-10-21 00:27:46 +0800

    expansionPanelList

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'HyperSpectral'),
+      home: const MyHomePage(title: 'HyperSpectral Imaging'),
     );
   }
 }
@@ -49,15 +49,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool _isLiveview = false;
+  bool _isExpanded = false;
 
-  void _incrementCounter() {
+  void _liveviewSwitchOnChanged(bool valueChanged) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLiveview = valueChanged;
     });
   }
 
@@ -70,46 +67,76 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SingleChildScrollView(
+          child: ExpansionPanelList(
+              children: [
+                ExpansionPanel(
+                    headerBuilder: (BuildContext context, _isExpanded) {
+                      return const ListTile(
+                        title: Text("Imaging"),
+                        leading: Icon(
+                          Icons.camera_alt_rounded,
+                          color: Colors.amber,
+                        ),
+                      );
+                    },
+                    body: ListTile(
+                      leading: Icon(Icons.camera),
+                      subtitle: Column(
+                        children: [
+                          Row(children: [
+                            Switch(
+                                value: _isLiveview,
+                                onChanged: _liveviewSwitchOnChanged),
+                            Text("Liveview")
+                          ]),
+                          Row(children: [
+                            Switch(
+                                value: _isLiveview,
+                                onChanged: _liveviewSwitchOnChanged),
+                            Text("OM")
+                          ]),
+                          Switch(
+                              value: _isLiveview,
+                              onChanged: _liveviewSwitchOnChanged),
+                          Switch(
+                              value: _isLiveview,
+                              onChanged: _liveviewSwitchOnChanged),
+                        ],
+                      ),
+                    ),
+                    isExpanded: _isExpanded),
+                ExpansionPanel(
+                    headerBuilder: (BuildContext context, _isExpanded) {
+                      return const ListTile(
+                        title: Text("Stage"),
+                      );
+                    },
+                    body: ListTile(
+                      leading: Icon(Icons.camera),
+                      subtitle: Column(
+                        children: [
+                          Switch(
+                            value: _isLiveview,
+                            onChanged: _liveviewSwitchOnChanged,
+                            activeColor: Colors.amber,
+                          ),
+                        ],
+                      ),
+                    ),
+                    isExpanded: _isExpanded),
+              ],
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  _isExpanded = !isExpanded;
+                });
+              },
+              animationDuration: const Duration(milliseconds: 150)),
+        ));
   }
 }
